4d804662ad4d613405616e219e979a74f77c1a0c
aryan <aryan.iitgn@gmail.com>
2026-01-03 00:27:21 +0530

    fixed

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 window.APP_CONFIG = {
-    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzmiMnrxkRfLpbK7SahzgbF0lFqmTufkgjka_VCU2VtuiyP_1x19uj2QXgO7JVAQQat/exec"
+    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwkIYk2Qf4DwHsgz2RMjBeBbZvxyc5K7g08BhZRurwBpe3UW3s0AEGKvsaUxrfE73XmUw/exec"
 };
