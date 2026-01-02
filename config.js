72d88cc023936c1e1d56dbbeefc87ea041abddad
aryan <aryan.iitgn@gmail.com>
2026-01-03 00:22:11 +0530

    fixed

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 window.APP_CONFIG = {
-    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycby6PXJE0YljtK7d6nv_t5OJeIGhBALXvVbqMACLpazGOBYHTuDsqkKx8SdsQKT0nq8zUQ/exec"
+    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzmiMnrxkRfLpbK7SahzgbF0lFqmTufkgjka_VCU2VtuiyP_1x19uj2QXgO7JVAQQat/exec"
 };
