a1b50e050bf5e42964c4d3232835f93f8602a60e
aryan <aryan.iitgn@gmail.com>
2026-01-03 00:01:34 +0530

    updated latest app_script_url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 window.APP_CONFIG = {
-    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyDtoI6AF7gQm016DbnLPcGiylLcu0IakPvaIP43UX19B447OybZj8zwSFKStBSqbVjaA/exec"
+    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycby6PXJE0YljtK7d6nv_t5OJeIGhBALXvVbqMACLpazGOBYHTuDsqkKx8SdsQKT0nq8zUQ/exec"
 };
