9319e1152d14f63357f288fadbae0d50ec031e96
aryan <aryan.iitgn@gmail.com>
2026-01-10 13:37:02 +0530

    changed appscirpt url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 window.APP_CONFIG = {
-    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwkIYk2Qf4DwHsgz2RMjBeBbZvxyc5K7g08BhZRurwBpe3UW3s0AEGKvsaUxrfE73XmUw/exec"
+    APPS_SCRIPT_URL: "https://api.purnamyogashala.com/submit-form"
 };
